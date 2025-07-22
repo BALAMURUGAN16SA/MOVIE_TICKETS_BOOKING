69ebd02fbf49017e5d76f33e3d77da84ade5af23
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2025-07-22 19:52:06 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,38 +1,26 @@
 import psycopg2 as dbm
 from psycopg2.extras import RealDictCursor
+import os
 import config
 
-con = None
-cur = None
-
 def db_connect():
-    con = dbm.connect(
+    database_url = os.environ.get("DATABASE_URL")
+
+    if database_url:
+        con = dbm.connect(database_url, cursor_factory=RealDictCursor)
+    else:
+        con = dbm.connect(
             user = config.user,
             password = config.password,
             host = config.host,
             port = config.port,
             dbname = config.dbname,
+            cursor_factory=RealDictCursor
         )
-    cur = con.cursor(cursor_factory = RealDictCursor)
-
-    return con, cur
-    # database_url = config.dburl
     
-    # if database_url:
-    #     con = dbm.connect(database_url)
-    #     cur = con.cursor(cursor_factory=RealDictCursor)
-    # else:
-    #     con = dbm.connect(
-    #         user = config.user,
-    #         password = config.password,
-    #         host = config.host,
-    #         port = config.port,
-    #         dbname = config.dbname,
-    #     )
-    #     cur = con.cursor(cursor_factory = RealDictCursor)
+    cur = con.cursor()
+    return con, cur
 
-    # return con, cur
-
-def db_close():
-    con.close()
+def db_close(con, cur):
     cur.close()
+    con.close()
