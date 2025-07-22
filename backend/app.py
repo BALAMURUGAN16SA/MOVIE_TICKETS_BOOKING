12df324fbf0d9ca68c4a951138ba15ae6e150025
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2025-07-22 21:05:08 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 app.register_blueprint(user_bp, url_prefix = '/user')
 app.register_blueprint(admin_bp, url_prefix = '/admin')
 
-CORS(app, origins=["http://localhost:3000", "https://movie-tickets-booking-8bn9.onrender.com"])
+CORS(user_bp, origins=["http://localhost:3000"])
+CORS(admin_bp, origins=["http://localhost:3000"])
 JWT_SECRET_KEY = config.jwt_secret_key
 JWT_REFRESH_KEY = config.jwt_refresh_key
 
