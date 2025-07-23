58d1d19d184cbe42fca2dbc4261226f9af30279b
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2025-07-23 10:07:34 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,3 @@
 import os
-# user="postgres"
-# password="Yellow123*"
-# dbname="imtbs"
-# host="localhost"
-# port=5432
 jwt_secret_key = os.getenv("JWT_SECRET_KEY")
 jwt_refresh_key = os.getenv("JWT_REFRESH_KEY")
