3bf13e2ce3d6b3376570c05613ce5a25d2b7e9ac
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2025-07-25 11:55:14 +0530

    Updated

--- frontend-react/src/components/Login.js
+++ frontend-react/src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Form, Button, Card, CloseButton, Toast, ToastContainer } from 'react-bootstrap';
 import { FaGoogle, FaLock, FaUser } from 'react-icons/fa';
 import { useAuth } from './Auth';
